b54fc54b65ef63c383c9a90ce339e8e7a2aab7a6
Mikhail <42357179+MikMech@users.noreply.github.com>
2021-12-16 15:31:16 -0600

    working solution. needed to check data types

--- day1_1.ps1
+++ day1_1.ps1
@@ -1,17 +1,11 @@
-$day1input1 = Get-Content -Path "C:\Users\Mikhail\Documents\HomeAutomation\CodeAdvent\Day1Input1.txt" 
-$currentday = 0
+$advent_input = Get-Content -Path "C:\Users\Mikhail\Documents\HomeAutomation\CodeAdvent\Day1Input1.txt" 
 $increasecnt = 0
-$limit = $day1input1.Count
-while ($currentday -le $limit) {
-
-    $next = $currentday + 1
-    if ($day1input1[$currentday] -lt $day1input1[$next]) {
+$current = 0
+foreach ($line in $advent_input) {
+    $last = $current - 1
+    if ([int]$advent_input[$current] -gt [int]$advent_input[$last]) {
         $increasecnt++
-        $currentday++
     }
-    else {
-        $currentday++
-    }
-Write-Output $increasecnt
+    $current++
 }
 $increasecnt
